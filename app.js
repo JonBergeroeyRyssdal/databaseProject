1b43e5c9f42f608ca05e128a4363e1587b5b1a51
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2025-02-22 15:11:18 +0100

    before adding orange when room is reserved

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ db.sequelize.sync({ force: false })
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hotelsRouter = require('./routes/hotels');
+var roomsRouter = require('./routes/rooms');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hotels', hotelsRouter);
+app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
